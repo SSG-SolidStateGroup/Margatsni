f236ddef6b299a154e08ad5f3ee675f4bc35b405
ieabbas <the0nlyismail@gmail.com>
2017-10-08 18:17:39 -0700

    Added HTTP API test

---
+++ flaskFrameworkTest.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def flaskFrameworkTest():
+    return "This is a working example of the ssg HTTP API code"
+
+if __name__ == "__main__":
+    app.run(debug = True)
