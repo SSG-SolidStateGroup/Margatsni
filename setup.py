4cd02e43da5ea5764983234bbfebe2feeb83fdf2
Ismail A <ieabbas@users.noreply.github.com>
2017-10-12 14:32:29 -0700

    Deleted leftover Django information

--- setup.py
+++ setup.py
@@ -35,19 +35,3 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
-
-
-if overlay_warning:
-    sys.stderr.write("""
-========
-WARNING!
-========
-You have just installed Django over top of an existing
-installation, without removing it first. Because of this,
-your install may now include extraneous files from a
-previous version that have since been removed from
-Django. This is known to cause a variety of problems. You
-should manually remove the
-%(existing_path)s
-directory and re-install Django.
-""" % {"existing_path": existing_path})
