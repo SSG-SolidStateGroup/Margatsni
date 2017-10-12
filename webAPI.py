015669ad42ac791e993e22597da6cda7c8a7b96c
Ismail A <ieabbas@users.noreply.github.com>
2017-10-12 14:00:45 -0700

    Moving assignments to assignment branch

--- webAPI.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def webApp():
-    return "SSG HTTP API Controller Example!"
-
-if __name__ == "__main__":
-    app.run(debug = True)
