3c6be0ade02e6cb6b196a9860484c981ace11467
Ismail A <ieabbas@users.noreply.github.com>
2017-10-12 14:15:04 -0700

    Pasted Django's setup.py for reference

--- setup.py
+++ setup.py
@@ -1,1 +1,57 @@
+import os
 
+setup(
+    name='Django',
+    version=version,
+    url='https://www.djangoproject.com/',
+    author='Django Software Foundation',
+    author_email='foundation@djangoproject.com',
+    description=('A high-level Python Web framework that encourages '
+                 'rapid development and clean, pragmatic design.'),
+    license='BSD',
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    include_package_data=True,
+    scripts=['django/bin/django-admin.py'],
+    entry_points={'console_scripts': [
+        'django-admin = django.core.management:execute_from_command_line',
+    ]},
+    install_requires=['pytz'],
+    extras_require={
+        "bcrypt": ["bcrypt"],
+        "argon2": ["argon2-cffi >= 16.1.0"],
+    },
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
+
+if overlay_warning:
+    sys.stderr.write("""
+========
+WARNING!
+========
+You have just installed Django over top of an existing
+installation, without removing it first. Because of this,
+your install may now include extraneous files from a
+previous version that have since been removed from
+Django. This is known to cause a variety of problems. You
+should manually remove the
+%(existing_path)s
+directory and re-install Django.
+""" % {"existing_path": existing_path})
