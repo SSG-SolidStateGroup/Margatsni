d22298d4ff3f720f034c92c0e81b99872b610805
Ismail A <ieabbas@users.noreply.github.com>
2017-10-12 14:31:45 -0700

    Updated Setup.py with basic Margatsni information

--- setup.py
+++ setup.py
@@ -1,40 +1,36 @@
 import os
-
+### Things that are commented are things I'm not sure what they do yet (- Ismail A.)
 setup(
-    name='Django',
-    version=version,
-    url='https://www.djangoproject.com/',
-    author='Django Software Foundation',
-    author_email='foundation@djangoproject.com',
-    description=('A high-level Python Web framework that encourages '
-                 'rapid development and clean, pragmatic design.'),
-    license='BSD',
-    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    name='Margasni',
+    version='1.0',
+    url='http://cs480-projects.github.io/teams-fall2017/SSG/index.html',
+    author='Solid State Group',
+    # author_email='foundation@djangoproject.com',
+    description=('An Instagram photo download tool),
+    # license='BSD',
+    # packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
     include_package_data=True,
-    scripts=['django/bin/django-admin.py'],
-    entry_points={'console_scripts': [
+    # scripts=['django/bin/django-admin.py'],
+    # entry_points={'console_scripts': [
         'django-admin = django.core.management:execute_from_command_line',
     ]},
-    install_requires=['pytz'],
-    extras_require={
-        "bcrypt": ["bcrypt"],
-        "argon2": ["argon2-cffi >= 16.1.0"],
-    },
+    # install_requires=['pytz'],
+    # extras_require={
+    #   "bcrypt": ["bcrypt"],
+    #    "argon2": ["argon2-cffi >= 16.1.0"],
+    # },
     zip_safe=False,
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
+        'Framework :: Flask',
+        'Intended Audience :: Instagram Users',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Internet :: WWW/HTTP :: WSGI',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
