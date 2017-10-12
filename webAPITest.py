3ab51e6eeb959888581e8eefc31b38d7e886df24
Ismail A <ieabbas@users.noreply.github.com>
2017-10-12 01:20:18 -0700

    Update and rename flaskFrameworkTest.py to webAPITest.py
    - Preparing for switchover to Ubuntu

--- webAPITest.py
+++ webAPITest.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def flaskFrameworkTest():
+def webAPITest():
     return "This is a working example of the ssg HTTP API code"
 
 if __name__ == "__main__":
