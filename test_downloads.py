bc64196429e8a48a538d8bebdb4b3e65cc78496e
calvinteng <calvint101@gmail.com>
2017-11-01 15:28:51 -0700

    still creating test cases

--- test_downloads.py
+++ test_downloads.py
@@ -1,14 +1,12 @@
 from instagram.client import InstagramAPI
-import pytest
-import os
-import requests
-import shutil
+import os, requests, shutil, unittest
 
 access_token = "1452718484.7aaf07a.512deb47397e4bffb6e18da65f11270e"
 client_secret = "67a95b790d714163bf039758690a9b60"
 client_id = "7aaf07aaa882491eb4c73bbdc94fa455"
 
 api = InstagramAPI(access_token=access_token, client_secret=client_secret, client_id=client_id)
+
 
 def get_self_recent_media():
 	next_max = 3
@@ -25,16 +23,7 @@ def get_self_recent_media():
 		for media in user_media_feed:
 			photos.append(media.get_standard_resolution_url())
 		counter += 1
-
 	return photos
-
-def return_file(photos = []):
-	get_file = ''.join(('/zip_files/', download(photos)))
-	dir_path = os.path.dirname(os.path.realpath(__file__))
-	dir_path = ''.join((dir_path, '/..'))
-	dir_path = ''.join((dir_path, get_file))
-	fname = str(session['instagram_user']['username']) + '.zip'
-	return send_file(filename_or_fp=dir_path, as_attachment=True, attachment_filename=fname)
 
 def download(photo_urls):
 	username = "dl_test"
@@ -67,6 +56,15 @@ def download(photo_urls):
 					media_file.write(content)
 	return file_path
 
+'''
+def return_file(photos = []):
+	get_file = ''.join(('/zip_files/', download(photos)))
+	dir_path = os.path.dirname(os.path.realpath(__file__))
+	dir_path = ''.join((dir_path, '/..'))
+	dir_path = ''.join((dir_path, get_file))
+	fname = str(session['instagram_user']['username']) + '.zip'
+	return send_file(filename_or_fp=dir_path, as_attachment=True, attachment_filename=fname)
+
 def zip_files():
 	# makes .zip file with photos inside inside /zip_files/ directory
 	shutil.make_archive(username, 'zip', dl_dst)
@@ -79,10 +77,15 @@ def zip_files():
 		pass
 
 	return zip_fname
+'''
 
-def download_test():
-	photos = get_self_recent_media()
-	path = download(photos)
-	is_file_in_path = os.path.isfile(path)
-	shutil.rmtree("./downloads/dl_test/")
-	assert is_file_in_path
+class TestDownload(unittest.TestCase):
+	def test_downloads(self):
+		photos = get_self_recent_media()
+		path = download(photos)
+		is_file_in_path = os.path.isfile(path)
+		shutil.rmtree("./downloads/dl_test/")
+		self.assertTrue(is_file_in_path)
+
+if __name__ == '__main__':
+	unittest.main()
