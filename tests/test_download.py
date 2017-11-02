20e10341a992c7813d02b6b7cc530f3bc6442b02
Calvin Teng <calvint101@gmail.com>
2017-11-01 21:35:06 -0700

    asdifjoaisjdf

--- tests/test_download.py
+++ tests/test_download.py
@@ -61,10 +61,10 @@ def zip_files():
 	shutil.make_archive('dl_test', 'zip', dl_dst)
 
 	try:
-		shutil.move('dl_test.zip', '../zip_files/dl_test.zip')
+		shutil.move('dl_test.zip', './zip_files/dl_test.zip')
 	except shutil.Error:
 		os.remove('../zip_files/' + 'dl_test.zip')
-		shutil.move('dl_test.zip', '../zip_files/dl_test.zip')
+		shutil.move('dl_test.zip', './zip_files/dl_test.zip')
 		pass
 
 class TestDownload(unittest.TestCase):
