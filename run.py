4133284b87b1e99cab85d4b2fe2128004649f711
Calvin Teng <calvint101@gmail.com>
2017-11-04 11:36:04 -0700

    testing logs

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from Margatsni import app
 
 if __name__ == '__main__':
+	import logging
+    logging.basicConfig(filename='error.log',level=logging.DEBUG)
+    
 	app.run(host='0.0.0.0', port=80)
 	#app.run(debug=True)
