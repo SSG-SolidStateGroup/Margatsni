07d310902723cd7d93e11acc15b328f45af1b9e8
Calvin Teng <calvint101@gmail.com>
2017-11-08 01:09:09 -0800

    changed up index.html, no need for index2.html, downloads work so far, but zipping files does not work at least on my machine. done for today

--- Margatsni/views.py
+++ Margatsni/views.py
@@ -1,60 +1,81 @@
 from Margatsni import app
 from instagram.client import InstagramAPI
-
-from flask import Flask, request, render_template, session, redirect, send_file
-import os, requests, shutil, json
+from instagram_scraper import InstagramScraper
+from flask import Flask, request, render_template, session, redirect, flash, send_file
+import os, requests, shutil, json, concurrent.futures
 
 LOGIN_URL = "https://www.instagram.com/accounts/login/ajax/"
+max_items = 20
+api = InstagramScraper(login_user=None, login_pass=None, media_types=['image','carousel'], maximum=max_items)
+
 # main page
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+# log-in page, will detect invalid logins
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = None
+	failed_login = False
+	session['logged_in'] = False
 	if request.method == 'POST':
 		session['login_user'] = request.form['username']
 		session['login_pass'] = request.form['password']
-		s = requests.Session()
+		login_text, login = validateUser()
 
-		s.headers.update({'Referer': "https://www.instagram.com"})
-		req = s.get("https://www.instagram.com")
-		s.headers.update({'X-CSRFToken': req.cookies['csrftoken']})
-
-		login_data = {'username': session['login_user'], 'password': session['login_pass']}
-		login = s.post(LOGIN_URL, data=login_data, allow_redirects=True)
-		s.headers.update({'X-CSRFToken': login.cookies['csrftoken']})
-		login_text = json.loads(login.text)
 		if login_text.get('authenticated') and login.status_code == 200:
-			return render_template('index2.html')
+			api.login_user = session['login_user']
+			api.login_pass = session['login_pass']
+			api.login()
+			session['logged_in'] = True
+			return redirect('/')
 		else:
-			return render_template('index.html')
+			flash('Unsuccessful login.')
 	return render_template('login.html')
 
-@app.route('/get-user-media', methods=['GET', 'POST'])
-def get_self_recent_media():
-	next_max = 3
-	api = InstagramAPI(access_token=session['instagram_access_token'])
-	user_media_feed, next = api.user_media_feed()
+# validates if user and pass is a valid instagram account
+def validateUser():
+	s = requests.Session()
+	s.headers.update({'Referer': "https://www.instagram.com"})
+	req = s.get("https://www.instagram.com")
+	s.headers.update({'X-CSRFToken': req.cookies['csrftoken']})
+	login_data = {'username': session['login_user'], 'password': session['login_pass']}
+	login = s.post(LOGIN_URL, data=login_data, allow_redirects=True)
+	s.headers.update({'X-CSRFToken': login.cookies['csrftoken']})
+	return json.loads(login.text), login
 
-	photos = []
-	for media in user_media_feed:
-		photos.append(media.get_standard_resolution_url())
+@app.route('/get-target-media', methods=['GET', 'POST'])
+def get_target_media():
+	executor=concurrent.futures.ThreadPoolExecutor(max_workers=10)
 
-	counter = 1
-	while next and counter < next_max:
-		user_media_feed, next = api.user_media_feed(with_next_url=next)
-		for media in user_media_feed:
-			photos.append(media.get_standard_resolution_url())
-		counter += 1
+	username = api.username = 'kellynieh'
+	zip_fname = username + '.zip'
+	api.posts = []
+	api.last_scraped_filetime = 0
+	future_to_item = {}
 
-	get_file = ''.join(('/zip_files/', download(photos)))
-	dir_path = os.path.dirname(os.path.realpath(__file__))
-	dir_path = ''.join((dir_path, '/..'))
-	dir_path = ''.join((dir_path, get_file))
-	fname = str(session['instagram_user']['username']) + '.zip'
-	return send_file(filename_or_fp=dir_path, as_attachment=True, attachment_filename=fname)
+	dst = './downloads/' + username
+	try:
+		os.makedirs(dst)
+	except FileExistsError:
+		shutil.rmtree(dst)
+		os.makedirs(dst)
+		pass
+
+	user_details = api.get_user_details(username)
+
+	# Crawls the media and sends it to the executor.
+	api.get_media(dst, executor, future_to_item, user_details)
+
+	shutil.make_archive(username, 'zip', dst)
+
+	try:
+		shutil.move(zip_fname, './zip_files/' + zip_fname)
+	except shutil.Error:
+		os.remove('./zip_files/' + zip_fname)
+		shutil.move(zip_fname, './zip_files/' + zip_fname)
+		pass
+	return send_file(filename_or_fp='../zip_files/'+zip_fname, as_attachment=True, attachment_filename=zip_fname)
 
 def download(photo_urls):
 	username = str(session['instagram_user']['username'])
