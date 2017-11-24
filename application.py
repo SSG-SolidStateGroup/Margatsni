457de40006dd15d046d40b49e33ce9a7f156eb5f
Calvin Teng <calvint101@gmail.com>
2017-11-24 11:46:39 -0800

    test

--- application.py
+++ application.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
 	logging.basicConfig(filename='error.log',level=logging.DEBUG)
 	scheduler.init_app(application)
 	scheduler.start()
-	application.run(host='0.0.0.0', port=80)
-	#application.run(debug=True)
+	#application.run(host='0.0.0.0', port=80)
+	application.run(debug=True)
