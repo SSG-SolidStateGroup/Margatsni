0a778faec673673a50c7a402c9e668c8ba78dfb3
Calvin Teng <calvint101@gmail.com>
2017-11-24 12:59:07 -0800

    still trying eb

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
-from Margatsni import application, scheduler
+from app import application, scheduler
 
 if __name__ == '__main__':
 	import logging
 	logging.basicConfig(filename='error.log',level=logging.DEBUG)
 	scheduler.init_app(application)
 	scheduler.start()
-	#application.run(host='0.0.0.0', port=80)
-	application.run(debug=True)
+	application.run(host='0.0.0.0', port=80)
+	#application.run(debug=True)
