f89874351aa0be318c6797bf647c2e59404823d9
Calvin Teng <calvint101@gmail.com>
2018-06-16 02:51:10 -0700

    attempt to fix crash

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from Margatsni import application, scheduler
 
 if __name__ == '__main__':
-	import logging
-	logging.basicConfig(filename='error.log',level=logging.DEBUG)
+	#import logging
+	#logging.basicConfig(filename='error.log',level=logging.DEBUG)
 	scheduler.init_app(application)
 	scheduler.start()
 	application.run(host='0.0.0.0', port=80)
